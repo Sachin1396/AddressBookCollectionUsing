c15d4e441c346daab085c46da0187fe65f16f74f
Sachin1396 <sachhushetty1396@gmail.com>
2022-07-25 12:21:11 +0530

    Refactor:multiple address book

--- AddressBook.java
+++ AddressBook.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 public class AddressBook {
     Contacts contacts;
     Scanner scanner = new Scanner(System.in);
-    ArrayList<Contacts> arrayList = new ArrayList<Contacts>();
-    public void addContact(){
-       // Scanner scanner = new Scanner(System.in);
+    ArrayList<Contacts> list = new ArrayList<Contacts>();
+    AddressBookMain addressBookMain = new AddressBookMain();
+
+    public void addContact() {
+        // Scanner scanner = new Scanner(System.in);
         contacts = new Contacts();
         System.out.println("Enter the first name");
         contacts.setFirstName(scanner.nextLine());
@@ -23,51 +25,57 @@ public class AddressBook {
         System.out.println("enter the phone number");
         contacts.setPhoneNumber(scanner.nextLine());
         //System.out.println(contacts);
-        arrayList.add(contacts);
-        //System.out.println(contacts);
+        list.add(contacts);
+        System.out.println(contacts);
     }
-    public void displayContact()
-    {
-        for(int i=0;i<arrayList.size();i++){
-            System.out.println(arrayList.get(i)+"");
+
+    public void displayContact() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i) + "");
         }
+        System.out.println(AddressBookMain.addressBookMap);
     }
+
     //editing contact based on person name
     public void editContact() {
         System.out.println("Enter the first name");
         String firstName = scanner.nextLine();
-        if (firstName.equalsIgnoreCase(contacts.getFirstName())) {
-            System.out.println("Enter the first name");
-            contacts.setFirstName(scanner.nextLine());
-            System.out.println("enter the last name");
-            contacts.setLastName(scanner.nextLine());
-            System.out.println("enter the city");
-            contacts.setCity(scanner.nextLine());
-            System.out.println("enter the state");
-            contacts.setState(scanner.nextLine());
-            System.out.println("enter the zipcode");
-            contacts.setZipCode(scanner.nextLine());
-            System.out.println("eneter the email");
-            contacts.setEmail(scanner.nextLine());
-            System.out.println("enter the phone number");
-            contacts.setPhoneNumber(scanner.nextLine());
-            System.out.println(contacts);
-        } else {
-            System.out.println("contact is not found");
+        for (Contacts contacts : list) {
+            if (firstName.equalsIgnoreCase(contacts.getFirstName())) {
+                System.out.println("Enter the first name");
+                contacts.setFirstName(scanner.nextLine());
+                System.out.println("enter the last name");
+                contacts.setLastName(scanner.nextLine());
+                System.out.println("enter the city");
+                contacts.setCity(scanner.nextLine());
+                System.out.println("enter the state");
+                contacts.setState(scanner.nextLine());
+                System.out.println("enter the zipcode");
+                contacts.setZipCode(scanner.nextLine());
+                System.out.println("eneter the email");
+                contacts.setEmail(scanner.nextLine());
+                System.out.println("enter the phone number");
+                contacts.setPhoneNumber(scanner.nextLine());
+                //System.out.println(contacts);
+            } else {
+                System.out.println("contact is not found");
+            }
         }
     }
         //Deleting the contact based on firstName
-        public void deleteContact(){
-            System.out.println("enter the first name");
-            String firstName = scanner.nextLine();
-            if(firstName.equalsIgnoreCase(contacts.getFirstName())){
-                System.out.println("deleting the contact list as first name matched ");
-                contacts = null;
+        public void deleteContact() {
+            System.out.println("Enter the first name");
+            String firstName = scanner.next();
 
-            }
-            else {
-                System.out.println("contact not deleted as first name not matched ");
+            for (Contacts contacts : list) {
+                if (firstName.equalsIgnoreCase(contacts.getFirstName())) {
+                    System.out.println("Contact Deleted ");
+                    contacts = null;
+                } else {
+                    System.out.println("contact not deleted as first name not matched ");
+                }
             }
         }
     }
+
 
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,12 +1,30 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AddressBookMain {
+
+
+       static HashMap<String, AddressBook> addressBookMap = new HashMap<>();
+        public static void addNewAddressBook(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("enter the address book name");
+            String addressBookName = scanner.nextLine();
+            if(addressBookMap.containsKey(addressBookName)){
+                System.out.println("address book is already available");
+            }
+            else{
+                AddressBook addressBook =new AddressBook();
+                addressBookMap.put(addressBookName,addressBook);
+            }
+
+        }
     public static  void main(String[] args){
         System.out.println("Welcome to a address book program");
         AddressBook addressBook = new AddressBook();
 
         int choice;
         do{
+            addNewAddressBook();
             Scanner scanner = new Scanner(System.in);
             System.out.println("Kindly enter the choice");
             choice = scanner.nextInt();
